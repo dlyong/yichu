1c490fc763a7d3bff9e3a06d30aa1bdfa6f58d74
dlyong <zy_dl_0921@163.com>
2020-11-12 21:43:56 +0800

    添加spring security 配置未完待续,login 相关功能

--- yichu-base/src/main/java/com/dlyong/yichu/domain/baseuser/entity/BaseUser.java
+++ yichu-base/src/main/java/com/dlyong/yichu/domain/baseuser/entity/BaseUser.java
@@ -1,12 +1,17 @@
 package com.dlyong.yichu.domain.baseuser.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class BaseUser implements Serializable {
-    private String id;
+
+
+    @TableId(type = IdType.ASSIGN_ID) // 生成雪花算法id
+    private Long id;
 
     private String username;
 
